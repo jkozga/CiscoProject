d573e8edfd85871b7efa234c09c01a23d9c3c021
jk.ozga@gmail.com <jk.ozga@gmail.com>
2017-03-05 16:22:05 +0100

    Class

--- PythonCisco1/PythonCisco1.py
+++ PythonCisco1/PythonCisco1.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 import requests
 import json
 import sys
+
+requests.packages.urllib3.disable_warnings()
 
 username = 'cisco'
 password = 'cisco'
@@ -9,6 +13,16 @@ port = '55443'
 headers = {'Content-Type': 'application/vnd.yang.data+json',
            'Accept': 'application/json'}
 content_type="application/json"
+
+class Devices:
+    type = 'none'
+    host = 'none'
+    port = 0
+    def __init__(self, type, host, port):
+        self.port = port
+        self.host = host
+        self.type = type
+        return super().__init__()
 
 def checkCodeCorrect(code):
     if code == 200:
@@ -21,29 +35,51 @@ def checkCodeCorrect(code):
 def getToken():
     uri = 'https://{}:{}/api/v1/auth/token-services'.format(host, port)
 
-    response = requests.post(uri,auth = (username, password), headers = headers, verify = False)
+    response = requests.post(uri,auth = (username, password), headers = headers, verify = False, timeout=1)
     if checkCodeCorrect(response.status_code):
-        return (response.json())["token-id"] 
-    else:
-        return False
+        return (response.json())['token-id'] 
+    return False
+
+def getTokenDevice(host2, port2):
+    uri = 'https://{}:{}/api/v1/auth/token-services'.format(host2, port2)
+
+    response = requests.post(uri,auth = (username, password), headers = headers, verify = False, timeout=1)
+    if checkCodeCorrect(response.status_code):
+        return (response.json())['token-id'] 
+    return False
 
 def getBgpNeighbors(token, asn, vrf):
     headers = {"content-type": "application/json", "X-Auth-Token":token}
     uri = 'https://{}:{}/api/v1/vrf/{}/routing-svc/bgp/{}/neighbors'.format(host, port, vrf, asn)
     
-    response = requests.get(uri, headers=headers, verify=False)
+    response = requests.get(uri, headers=headers, verify=False, timeout=10)
     if checkCodeCorrect(response.status_code):
-        return (response.json())["items"]
-    else:
-        return False
+        return (response.json())['items']
+    return False
+
+def getConfiguredInterfaces(token):
+    headers = {"content-type": "application/json", "X-Auth-Token":token}
+    url = 'https://{}:{}/api/v1/interfaces'.format(host, port)
+
+    response = requests.get(url, headers=headers, verify=False, timeout=10)
+    if checkCodeCorrect(response.status_code):
+        return (response.json())['items']
+    return False
+
+def getConfiguredInterfacesDevice(token, host2, port2):
+    headers = {"content-type": "application/json", "X-Auth-Token":token}
+    url = 'https://{}:{}/api/v1/interfaces'.format(host2, port2)
+
+    response = requests.get(url, headers=headers, verify=False, timeout=10)
+    if checkCodeCorrect(response.status_code):
+        return (response.json())['items']
+    return False
 
 def setBGP(token, asn, vrf):
     headers = {"content-type": "application/json", "X-Auth-Token":token}
     uri = 'https://{}:{}/api/v1/vrf/{}/routing-svc/bgp'.format(host, port, vrf)
-    body = {   
-	    "routing-protocol-id": asn
-    }
-    response = requests.post(uri, data=json.dumps(body), headers=headers, verify=False)
+    body = {"routing-protocol-id": asn}
+    response = requests.post(uri, data=json.dumps(body), headers=headers, verify=False, timeout=1)
     #content = b'{"error-code": -1, "error-message": "BGP router 222 already exists in VRF a", "detail": " "}'
     if response.ok == True:
      print('BGP {} created in VRF {} !'.format(asn, vrf))
@@ -53,34 +89,30 @@ def setBGP(token, asn, vrf):
         if rresponse["error-code"] == -1:
             print(rresponse["error-message"])
         return False
-    
-def getConfiguredInterfaces(token):
-    headers = {"content-type": "application/json", "X-Auth-Token":token}
-    url = 'https://{}:{}/api/v1/interfaces'.format(host, port)
-
-
-    response = requests.get(url, headers=headers, verify=False)
-    if checkCodeCorrect(response.status_code):
-        return (response.json())["items"]
-    else:
-        return False
-
-
 
 def main():
 
-    token = getToken()
-    if token == False:
-        print('Issue with token')
-        return
+    #token = getToken()
+    #if token == False:
+    #    print('Issue with token')
+    #    return
 
-    setBGP(token,'222','b')
-    print('abc')
+    
+    #setBGP(token,'222','b')
+    #print('abc')
     #print(getBgpNeighbors(token, '222', 'a'))
     #interfaces = (getConfiguredInterfaces(token))
 
-    #for item in interfaces:
-    #    print(item['if-name'] + item['ip-address'])
+    #for interface in interfaces:
+    #    print(interface['if-name'] + '/' + interface['ip-address'])
+
+    devices = {'10.10.10.240', '10.10.10.241'}
+    for dev in devices:
+        d = Devices('asr', dev, 55443)
+        token = getTokenDevice(d.host, d.port)
+        interfaces = (getConfiguredInterfacesDevice(token, d.host, d.port))
+        for interface in interfaces:
+            print(interface['if-name'] + '/' + interface['ip-address'])
 
 if __name__ == '__main__':
     sys.exit(main())
